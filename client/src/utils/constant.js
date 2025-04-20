2e0370d3583f1ba8069d516f772215aa1ef3c267
Arnab <Narnabnath919@gmail.com>
2025-04-20 21:50:20 +0530

    chore: untrack constant.js and add to .gitignore

--- client/src/utils/constant.js
+++
@@ -1,5 +0,0 @@
-export const USER_API = "http://localhost:3000/api/user";
-export const JOB_API = "http://localhost:3000/api/job";
-export const COMPANY_API = "http://localhost:3000/api/company";
-export const APPLICATION_API = "http://localhost:3000/api/applications";
-export const NOTIFICATION_API = "http://localhost:3000/api/notification";
