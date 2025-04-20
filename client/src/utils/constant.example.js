15d4b9ba913d2ebac7327a564b8aab46943a7d4d
Arnab <Narnabnath919@gmail.com>
2025-04-20 21:43:17 +0530

    deployment added

---
+++ client/src/utils/constant.example.js
@@ -0,0 +1,6 @@
+// add your backend api for corresponding routes
+export const USER_API = "";
+export const JOB_API = "";
+export const COMPANY_API = "";
+export const APPLICATION_API = "";
+export const NOTIFICATION_API = "";
